b0ccc13f5c5459d02ea01f2ac670f6c5129689d9
basharovi <bashar.csediu@gmail.com>
2020-02-19 22:51:50 +0600

    initially ready

---
+++ SendingEmail/Controllers/EmailController.cs
@@ -0,0 +1,20 @@
+﻿using SendingEmail.Models;
+using System.Web.Mvc;
+
+namespace SendingEmail.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(EmailViewModel emailViewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ SendingEmail/Models/EmailViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SendingEmail.Models
+{
+    public class EmailViewModel
+    {
+        public string EmailSubject { get; set; }
+        public string EmailBody { get; set; }
+        public string ReceiverEmailAddress { get; set; }
+        public string SenderEmailAddress { get; set; }
+        public string SenderPassword { get; set; }
+        public string SmtpHostServer { get; set; }
+        public int SmtpPort { get; set; }
+    }
+}
